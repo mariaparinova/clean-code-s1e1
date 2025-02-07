a4c507d400201447d533db5cd8abd4cade9c22e9
Maria Parinova <maryia.parynava@gmail.com>
2025-02-07 17:34:55 +0100

    refactor (outside the rules): Improve .js readability: code nesting, add spaces

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-var taskInput=document.getElementById("new-task");
-var addButton=document.querySelector(".task__btn_add");
-var uncompletedTasks=document.getElementById("uncompleted");
-var completedTasks=document.getElementById("completed");
+var taskInput = document.getElementById("new-task");
+var addButton = document.querySelector(".task__btn_add");
+var uncompletedTasks = document.getElementById("uncompleted");
+var completedTasks = document.getElementById("completed");
 
-var createNewTaskElement=function(taskString){
-  var listItem=document.createElement("li");
-  var checkBox=document.createElement("input");
-  var label=document.createElement("label");
-  var editInput=document.createElement("input");
-  var editButton=document.createElement("button");
-  var deleteButton=document.createElement("button");
-  var deleteButtonImg=document.createElement("img");
+var createNewTaskElement = function(taskString) {
+  var listItem = document.createElement("li");
+  var checkBox = document.createElement("input");
+  var label = document.createElement("label");
+  var editInput = document.createElement("input");
+  var editButton = document.createElement("button");
+  var deleteButton = document.createElement("button");
+  var deleteButtonImg = document.createElement("img");
 
-  label.innerText=taskString;
-  label.className="task__label";
+  label.innerText = taskString;
+  label.className = "task__label";
 
   listItem.className = "task__item";
 
-  checkBox.type="checkbox";
-  checkBox.className="task__checkbox";
-  editInput.type="text";
-  editInput.className="task__input task__input_new-task";
+  checkBox.type = "checkbox";
+  checkBox.className = "task__checkbox";
 
-  editButton.innerText="Edit";
-  editButton.className="task__btn task__btn_edit";
+  editInput.type = "text";
+  editInput.className = "task__input task__input_new-task";
 
-  deleteButton.className="task__btn task__btn_delete";
-  deleteButtonImg.src="./remove.svg";
+  editButton.innerText = "Edit";
+  editButton.className = "task__btn task__btn_edit";
+
+  deleteButton.className = "task__btn task__btn_delete";
+  deleteButtonImg.src = "./remove.svg";
   deleteButtonImg.className = "task__icon";
   deleteButton.appendChild(deleteButtonImg);
 
@@ -39,69 +40,74 @@ var createNewTaskElement=function(taskString){
   return listItem;
 }
 
-var addTask=function(){
-  if (!taskInput.value) return;
-  var listItem=createNewTaskElement(taskInput.value);
+var addTask = function() {
+  if (!taskInput.value) {
+    return;
+  }
+
+  var listItem = createNewTaskElement(taskInput.value);
 
   uncompletedTasks.appendChild(listItem);
   bindTaskEvents(listItem, taskCompleted);
 
-  taskInput.value="";
+  taskInput.value = "";
 }
 
-var editTask=function(){
-  var listItem=this.parentNode;
-  var editInput=listItem.querySelector(".task__input");
-  var label=listItem.querySelector(".task__label");
-  var editBtn=listItem.querySelector(".task__btn_edit");
-  var containsClass=listItem.classList.contains("tasks__item_edit-mode");
-  if(containsClass){
-    label.innerText=editInput.value;
-    editBtn.innerText="Edit";
-  }else{
-    editInput.value=label.innerText;
-    editBtn.innerText="Save";
+var editTask = function() {
+  var listItem = this.parentNode;
+  var editInput = listItem.querySelector(".task__input");
+  var label = listItem.querySelector(".task__label");
+  var editBtn = listItem.querySelector(".task__btn_edit");
+  var containsClass = listItem.classList.contains("tasks__item_edit-mode");
+
+  if(containsClass) {
+    label.innerText = editInput.value;
+    editBtn.innerText = "Edit";
+  } else {
+    editInput.value = label.innerText;
+    editBtn.innerText = "Save";
   }
 
   listItem.classList.toggle("tasks__item_edit-mode");
 };
 
-var deleteTask=function(){
-  var listItem=this.parentNode;
-  var ul=listItem.parentNode;
+var deleteTask = function() {
+  var listItem = this.parentNode;
+  var ul = listItem.parentNode;
 
   ul.removeChild(listItem);
 }
 
-var taskCompleted=function(){
-  var listItem=this.parentNode;
+var taskCompleted = function() {
+  var listItem = this.parentNode;
 
   completedTasks.appendChild(listItem);
   bindTaskEvents(listItem, taskUncompleted);
 }
 
-var taskUncompleted=function(){
-  var listItem=this.parentNode;
+var taskUncompleted = function() {
+  var listItem = this.parentNode;
+
   uncompletedTasks.appendChild(listItem);
-  bindTaskEvents(listItem,taskCompleted);
+  bindTaskEvents(listItem, taskCompleted);
 }
 
-addButton.addEventListener("click",addTask);
+addButton.addEventListener("click", addTask);
 
-var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
-  var checkBox=taskListItem.querySelector(".task__checkbox");
-  var editButton=taskListItem.querySelector(".task__btn_edit");
-  var deleteButton=taskListItem.querySelector(".task__btn_delete");
+var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
+  var checkBox = taskListItem.querySelector(".task__checkbox");
+  var editButton = taskListItem.querySelector(".task__btn_edit");
+  var deleteButton = taskListItem.querySelector(".task__btn_delete");
 
-  editButton.onclick=editTask;
-  deleteButton.onclick=deleteTask;
-  checkBox.onchange=checkBoxEventHandler;
+  editButton.onclick = editTask;
+  deleteButton.onclick = deleteTask;
+  checkBox.onchange = checkBoxEventHandler;
 }
 
-for (var i=0; i<uncompletedTasks.children.length;i++){
-  bindTaskEvents(uncompletedTasks.children[i],taskCompleted);
+for (var i = 0; i < uncompletedTasks.children.length; i++) {
+  bindTaskEvents(uncompletedTasks.children[i], taskCompleted);
 }
 
-for (var i=0; i<completedTasks.children.length;i++){
-  bindTaskEvents(completedTasks.children[i],taskUncompleted);
+for (var i = 0; i < completedTasks.children.length; i++) {
+  bindTaskEvents(completedTasks.children[i], taskUncompleted);
 }
